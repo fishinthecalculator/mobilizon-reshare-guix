c79a58d95c94b323fbd6d9d8dec6aa9f4fd24bd9
Giacomo Leidi <goodoldpaul@autistici.org>
2022-07-13 18:54:06 +0200

    Update build_all_versions.sh

--- build_all_versions.sh
+++ build_all_versions.sh
@@ -4,8 +4,12 @@ set -eo pipefail
 
 declare -a versions=("0.1.0"
                      "0.2.0"
-                     "0.2.2"
-                     "0.2.3"
+                     # 0.2.2 (and 0.2.3 as well) is the first release where the Guix dependencies
+                     # versions are actually compatible with pyproject.toml constraints. Because of that
+                     # the way the package it's built is kind of hacky and a near world rebuild for Guix.
+                     # 
+                     # While the packages are supposed to be buildable, we avoid building them in CI to
+                     # avoid incurring in Github's timout limitations
                      "0.3.0"
                      "0.3.1"
                      "0.3.2")
